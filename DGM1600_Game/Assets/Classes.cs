e51f99d9a26b210925b2ec614c591e3954054c70
Unknown <kendomorris@yahoo.com>
2017-08-30 20:25:36 -0600

    Classes

---
+++ DGM1600_Game/Assets/Classes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+	public class One {
+		public int money = 1000000;
+		public float fuel = 0.01f;
+		
+		private string name = "Bob";
+		
+		
+	}
+
+	public class two {
+		public int power = 50000;
+		public int vision = 5; 
+		public string script = "Wanna hear a joke?";
+		private string scriptTwo = "I bet you did, too bad.";
+
+
+	}
+
+
+
+	public class three {
+		public string message = "L, shitteiru no ka?";
+		public string messageTwo = "Shinigami wa";
+		private string messageThree = "ringo shika tabenai.";
+		
+
+
+
+	}
+}

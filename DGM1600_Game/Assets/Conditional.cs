efb83b1ded1f911c163ec34cafa9f1bfbf9ade54
Unknown <kendomorris@yahoo.com>
2017-09-11 20:00:47 -0600

    Assignment added : ElseClause

---
+++ DGM1600_Game/Assets/Conditional.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditional : MonoBehaviour {
+
+public string stopLight = "Red";
+
+void Start(){
+	Main();
+}
+void Main(){
+	if(stopLight == "Red"){
+	print("Stop! The light is red!");
+	}
+	else if (stopLight == "Yellow"){
+		print("Slow Down!");
+	}
+	else if (stopLight == "Green"){
+		print("Green means GO!");
+	}
+	else{
+		print("I dont know what you mean!");
+	}
+
+
+}
+
+
+}

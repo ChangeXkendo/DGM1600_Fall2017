ca82b070c664be07fc18b679aba3576412c9de49
Unknown <kendomorris@yahoo.com>
2017-09-11 20:16:14 -0600

    Assignment : Conventions_Syntax

---
+++ DGM1600_Game/Assets/Syntax_Conventions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Syntax_Conventions : MonoBehaviour {
+
+	public string myName = "Bob";
+
+	public int Money = 100;
+
+	private string Message = "You shall not pass.";
+}
+
+public class Syntax_Conventions2 : MonoBehaviour {
+
+	public float Power = 100000000000000;
+}
+
+
+//Syntax is the grammar that code used to make the computer understand it. It has to be perfect without mistake or things go haywire.//
+//A convention is the white space and indent usage to make the code easier for humans to read. It doesn't matter to the computer.//
